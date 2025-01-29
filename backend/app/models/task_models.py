c10856d111ce4cb377f03a9a1a057ecb82894baa
DNAdithya <niraja_adithya_dasireddi@outlook.com>
2025-01-29 13:10:25 +0530

    made changes in task models

--- backend/app/models/task_models.py
+++ backend/app/models/task_models.py
@@ -7,4 +7,33 @@ class TaskManHoursModel(BaseModel):
     Min: float
     Avg: float
     Est:float
+    
+
+class PartsUsageResponseModel(BaseModel):
+    Part_Id: str
+    PartDescription: str
+    Usage: dict
+
+class SkillsAnalysisModel(BaseModel):
+    Source_Task: list
+    
+class ManHours(BaseModel):
+    min: float
+    avg: float
+    max: float
+
+class SkillAnalysis(BaseModel):
+    skill: str
+    manHours: ManHours
+
+class TaskSkillAnalysis(BaseModel):
+    taskId: str
+    taskDescription: str
+    skills: list[SkillAnalysis]
+
+class FindingsSkillAnalysis(BaseModel):
+    taskId: str
+    skills: list[SkillAnalysis]
+    
+    
 
