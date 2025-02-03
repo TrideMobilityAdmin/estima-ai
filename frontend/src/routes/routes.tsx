ea828d8989cbdc05811b7e7a78fc79e78259e01a
sandhya <sandhya@tridemobility.com>
2025-02-03 17:05:52 +0530

    landing page, loginscreen, modules was created with  new ui

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,36 +1,65 @@
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import LandingPage from "../components/landingPage";
-import Basic from "../layout/Basic";
+import { Route, Routes } from "react-router-dom";
 import Login from "../layout/Login";
+import Landing from "../components/landing";
 import Estimate from "../views/estimate";
 import CompareEstimate from "../views/compareEstimate";
 import PartUsage from "../views/partUsage";
 import SkillRequirement from "../views/skillRequirement";
 import Configuration from "../views/configuration";
 
-
-
 const MainRoutes = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    return (
-        <>
-        <Routes>
-            <Route>
-                <Route path="/" element={<Login />} />
-                   <Route path="/home" element={<Basic />}>
-                   <Route path="/home" element={<LandingPage />} />
-                   <Route path="/home/estimate" element={<Estimate />} />
-                   <Route path="/home/compare-estimate" element={<CompareEstimate />} />
-                   <Route path="/home/part-usage" element={<PartUsage />} />
-                   <Route path="/home/skill-requirement" element={<SkillRequirement />} />
-                   <Route path="/home/configuration" element={<Configuration />} />
-                </Route>
-            </Route>
-        </Routes>
-        </>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Landing />}>
+        <Route path="/home/estimate" element={<Estimate />} />
+        <Route path="/home/compare-estimate" element={<CompareEstimate />} />
+        <Route path="/home/part-usage" element={<PartUsage />} />
+        <Route path="/home/skill-requirement" element={<SkillRequirement />} />
+        <Route path="/home/configuration" element={<Configuration />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default MainRoutes;
+
+
+// import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+// import LandingPage from "../components/landingPage";
+// import Basic from "../layout/Basic";
+// import Login from "../layout/Login";
+// import Estimate from "../views/estimate";
+// import CompareEstimate from "../views/compareEstimate";
+// import PartUsage from "../views/partUsage";
+// import SkillRequirement from "../views/skillRequirement";
+// import Configuration from "../views/configuration";
+// import Landing from "../components/landing";
+
+
+
+// const MainRoutes = () => {
+//     const navigate = useNavigate();
+//     const location = useLocation();
+
+//     return (
+//         <>
+//         <Routes>
+//             <Route>
+//                 <Route path="/" element={<Login />} />
+//                    <Route path="/home" element={<Landing />}>
+//                    {/* <Route path="/home" element={<LandingPage />} /> */}
+//                    {/* <Route path="/home" element={<Landing />} /> */}
+//                    <Route path="/home/estimate" element={<Estimate />} />
+//                    <Route path="/home/compare-estimate" element={<CompareEstimate />} />
+//                    <Route path="/home/part-usage" element={<PartUsage />} />
+//                    <Route path="/home/skill-requirement" element={<SkillRequirement />} />
+//                    <Route path="/home/configuration" element={<Configuration />} />
+//                 </Route>
+//             </Route>
+//         </Routes>
+//         </>
+//     )
+// }
+
+// export default MainRoutes;
