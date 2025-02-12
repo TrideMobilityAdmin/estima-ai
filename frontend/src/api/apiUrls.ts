9a0473fc39ac59a9f11b1b481ea66de044527229
mohith <mohit@tridemobility.com>
2025-02-12 12:49:46 +0530

    test

--- frontend/src/api/apiUrls.ts
+++ frontend/src/api/apiUrls.ts
@@ -8,3 +8,4 @@ export const getValidateTasks_Url = '/validate';
 export const getEstimateReport_Url = '/estimates/';
 export const getPartUsage_Url = '/parts/usage';
 export const getSkillReq_Url = '/skills/analysis';
+export const getSkill = '/skills/analysis';
