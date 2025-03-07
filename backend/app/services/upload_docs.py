c3c0f73b0e88ba3c80783db64e18fa04b46d2e79
radhika <radhika@gmail.com>
2025-03-07 19:03:16 +0530

    chnages in upload estimate

--- backend/app/services/upload_docs.py
+++ backend/app/services/upload_docs.py
@@ -522,14 +522,10 @@ class ExcelUploadService:
             "aircraftFlightHours": estimate_request.aircraftFlightHours,
             "aircraftFlightCycles": estimate_request.aircraftFlightCycles,
             "areaOfOperations": estimate_request.areaOfOperations,
-            # "cappingTypeManhrs": estimate_request.cappingTypeManhrs,
-            # "cappingManhrs": estimate_request.cappingManhrs,
-            # "cappingTypeSpareCost": estimate_request.cappingTypeSpareCost,
-            # "cappingSpareCost": estimate_request.cappingSpareCost,
+            
             "cappingDetails": estimate_request.cappingDetails.dict() if estimate_request.cappingDetails else None,
             "additionalTasks": [task.dict() for task in estimate_request.additionalTasks],
-            # "taskID": estimate_request.taskID,
-            # "taskDescription": estimate_request.taskDescription,
+           
             "miscLaborTasks": [task.dict() for task in estimate_request.miscLaborTasks]
 
             
